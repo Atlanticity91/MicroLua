16da336164c16c42a904081ae1d7bfba5b80de07
Atlanticity91 <Atlanticity91@gmail.com>
2024-12-17 16:39:31 +0100

    Setup documentation home page

--- Build/Build-Dependencies.lua
+++
@@ -1,5 +0,0 @@
---- THIRDPARTY LIBRARIES
-IncludeDirs = { }
-
-IncludeDirs[ "lua" ] = "%{wks.location}/Thirdparty/"
-IncludeDirs[ "test" ] = "%{wks.location}/MicroTest/"
--- Setup-Linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pushd ..
-
-echo === Micro Core Setup ===
-
-MicroCore/Build/Premake/Linux/premake5 --cc=clang --file=MicroCore/Build/Build.lua gmake2
-
-popd
